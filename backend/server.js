bd175832278036adc41c71c1770f44d2be2e23b8
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-09-24 04:04:40 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,24 @@ connectDB().catch(err => {
 });
 
 const app = express();
+const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Range', 'Set-Cookie'],
+};
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
